6bc7807934ef943048c48ba5eb6ff65d9a3c51f9
Kalana98 <it19005690@my.sliit.lk>
2025-02-19 11:54:40 +0530

    Added Order, Order Item, Payment Details, Order Status and Payment Status Entities and enums

---
+++ vendor hub/vendor-hub/src/main/java/com/nexora/domain/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.nexora.domain;
+
+public enum OrderStatus {
+
+    PENDING,
+    PLACED,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ vendor hub/vendor-hub/src/main/java/com/nexora/modal/OrderItem.java
@@ -0,0 +1,40 @@
+package com.nexora.modal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode
+
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private String size;
+
+    private String quantity;
+
+    private Integer mrpPrice;
+
+    private Integer sellingPrice;
+
+    private Long userId;
+
+
+}
